c901d55e4f8e348a659b4d6feb56c7c8432b7939
Kuang-Wen,Chou <zx011035@gmail.com>
2025-07-16 21:04:30 +0800

    完成products資料表ORM

---
+++ backend/models/products.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String,  Text, DECIMAL
+from sqlalchemy.orm import relationship
+from base import Base  # 從 base.py 匯入 Base，作為 ORM 基底類別
+
+class Products(Base):
+    __tablename__ = "products"  #對應資料表名稱
+    product_id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(100), nullable=False, comment="商品名稱")
+    price = Column(DECIMAL(10,2), nullable=False, comment="價格")
+    stock = Column(Integer, nullable=False, comment="庫存")
+    description = Column(Text, nullable=True, comment='描述')
+
+
