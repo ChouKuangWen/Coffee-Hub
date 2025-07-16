8080700ec226de1993b48aa385c241610e0f38a8
Kuang-Wen,Chou <zx011035@gmail.com>
2025-07-16 21:36:29 +0800

    完成orders資料表ORM

---
+++ backend/models/orders.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, String, Date, DECIMAL, ForeignKey, DateTime,  DEFAULT CURRENT_TIMESTAMP
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
+from base import Base  # 從 base.py 匯入 Base，作為 ORM 基底類別
+
+class Orders(Base):
+    __tablename__ = "orders"  #對應資料表名稱
+    order_id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.user_id"), nullable=False, comment="下單會員 ID")
+    status = Column(String(50), nullable=False, default="待付款", comment="訂單狀態")
+    total = Column(DECIMAL(10,2), ullable=False, comment="總金額")
+    created_at = Column(DateTime, default=func.now(), comment="建立時間")
+    status_updated_at = Column(DateTime, default=func.now(), comment="狀態變動時間")
+
+
