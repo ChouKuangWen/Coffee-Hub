24d8315e4975dfc6f25af9031df7665b1ca5a937
Kuang-Wen,Chou <zx011035@gmail.com>
2025-07-30 23:03:28 +0800

    自動從環境變數加載及型別驗證

---
+++ backend/app/core/config.py
@@ -0,0 +1,10 @@
+from pydantic_settings import BaseSettings  # 自動從環境變數加載及型別驗證
+
+class Settings(BaseSettings):
+    SECRET_KEY: str                   # 從 .env 讀取驗證金鑰
+    ACCESS_TOKEN_EXPIRE_MINUTES: int  # 從 .env 讀取過期時間
+
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
