04f83fd0b88c1eafab000c565c3512139275b2b4
Kuang-Wen,Chou <zx011035@gmail.com>
2025-08-11 20:42:57 +0800

    修正絕對路徑app.models.base

--- backend/app/models/used_jwt.py
+++ backend/app/models/used_jwt.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, Integer, String, DateTime
 from datetime import datetime
-from base import Base
+from app.models.base import Base
 
 class UsedJWT(Base):
     __tablename__ = "used_jwts"
