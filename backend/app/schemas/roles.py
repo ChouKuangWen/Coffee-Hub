4d530f74ff7dba6de4392028a479261229e938f0
Kuang-Wen,Chou <zx011035@gmail.com>
2025-08-14 09:30:19 +0800

    RoleRead內新增users: Optional[List["UserRead"]] = None  # 角色底下使用者

--- backend/app/schemas/roles.py
+++ backend/app/schemas/roles.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel      # 匯入 Pydantic 的基礎模型
 from typing import Optional, List, ForwardRef         # 匯入 Optional，允許欄位為可選
-
+from app.schemas.users import UserRead
 # 共用欄位基礎模型：用於其他 schema 繼承（避免重複）
 # 權限基本資料
 class PermissionBase(BaseModel):
@@ -21,7 +21,7 @@ class RoleRead(RoleBase):
     role_permissions: Optional[List["RolePermissionRead"]] = None
     #因為一個角色（Role）可以對應多個權限（Permissions）所以用list
     # 尚未建立RolePermissionRead，所以要用forward reference（前向引用)方法
-
+    users: Optional[List["UserRead"]] = None  # 角色底下使用者
     class Config:
         orm_mode = True
 
