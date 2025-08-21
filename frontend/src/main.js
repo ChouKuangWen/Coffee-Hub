a95b52187c002e5e439db70bb336613109d62282
Kuang-Wen,Chou <zx011035@gmail.com>
2025-08-21 11:42:24 +0800

    修改

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,15 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import "./index.css"; // TailwindCSS
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import { createPinia } from 'pinia';
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
 
 const app = createApp(App);
+const pinia = createPinia();
+
+app.use(pinia);
 app.use(router);
-app.mount("#app");
+app.use(ElementPlus);
+
+app.mount('#app');
