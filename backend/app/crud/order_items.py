fa041aa014e2ad38c2cee73747c399305273f8f8
Kuang-Wen,Chou <zx011035@gmail.com>
2025-08-26 11:44:34 +0800

    新增order_items.py  CRUD

---
+++ backend/app/crud/order_items.py
@@ -0,0 +1,33 @@
+# backend/app/crud/order_items.py
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select  
+from app.models.order_items import OrderItems
+
+async def get_order_item(db: AsyncSession, order_item_id: int):
+    result = await db.get(OrderItems, order_item_id)
+    return result
+
+async def get_all_order_items(db: AsyncSession):
+    result = await db.execute(
+        select(OrderItems)
+    )
+    return result.scalars().all()
+
+async def create_order_item(db: AsyncSession, order_item_data):
+    new_item = OrderItems(**order_item_data.dict())
+    db.add(new_item)
+    await db.commit()
+    await db.refresh(new_item)
+    return new_item
+
+async def update_order_item_info(db: AsyncSession, existing_item, order_item_data):
+    for field, value in order_item_data.dict(exclude_unset=True).items():
+        setattr(existing_item, field, value)
+    await db.commit()
+    await db.refresh(existing_item)
+    return existing_item
+
+async def delete_order_item(db: AsyncSession, existing_item):
+    await db.delete(existing_item)
+    await db.commit()
+    return existing_item
