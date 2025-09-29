6112106107f0101b956b3429484d819d45e1029f
Kuang-Wen,Chou <zx011035@gmail.com>
2025-09-29 23:49:46 +0800

    修改為用http cookie紀錄token

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -7,15 +7,18 @@ const api = axios.create({
   headers: {
     "Content-Type": "application/json",
   },
+  withCredentials: true, // 讓瀏覽器自動帶 Cookie
 })
 
-// 自動加上 Token
-api.interceptors.request.use(config => {
-  const token = localStorage.getItem("access_token")
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`
+api.interceptors.response.use(
+  response => response,
+  error => {
+    if (error.response?.status === 401) {
+      console.log("Token 過期或未登入，跳轉登入頁")
+      window.location.href = "/login"
+    }
+    return Promise.reject(error)
   }
-  return config
-})
+)
 
 export default api
