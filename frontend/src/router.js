f4a32937d467b4d7e32bc8acbf3f15acea4e3d48
Kuang-Wen,Chou <zx011035@gmail.com>
2025-10-02 20:14:40 +0800

    1.新增Orders訂單管理介面 2.改為 Cookie驗證加上 withCredentials

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -5,10 +5,10 @@ import Login from './views/Login.vue'
 import Dashboard from './views/Dashboard.vue'
 import Users from './views/Users.vue'
 import Register from './views/Register.vue'
-//import Orders from './views/Orders.vue'
+import Orders from './views/Orders.vue'
 //import Products from './views/Products.vue'
 //import Forbidden from './views/Forbidden.vue'
-import axios from 'axios'
+import api from './api'
 
 const routes = [
   { path: '/', redirect: '/home' },      // 預設導向Home首頁
@@ -16,7 +16,9 @@ const routes = [
   { path: '/login', component: Login },   // 登入頁
   { path: '/dashboard', component: Dashboard }, // 登入後的主頁
   { path: '/register', component: Register },   // 註冊頁
-  { path: '/users', component: Users, meta: { requiresRole: 1 } }  // 使用者查詢
+  { path: '/users', component: Users },  // 使用者查詢
+  { path: '/orders', component: Orders }  // 使用者查詢
+
 ]
 
 const router = createRouter({
@@ -27,7 +29,7 @@ const router = createRouter({
 
 // router 守衛
 router.beforeEach(async (to, from, next) => {
-  console.log("🔐 router 守衛啟動，目標路由:", to.path)
+  console.log("router 守衛啟動，目標路由:", to.path)
 
   // 公開頁面直接放行
   if (['/home', '/login', '/register'].includes(to.path)) {
@@ -36,23 +38,15 @@ router.beforeEach(async (to, from, next) => {
 
   try {
     // ✅ Cookie 版：加上 withCredentials
-    const res = await axios.get('http://localhost:8000/auth/me', {
+    const res = await api.get('http://localhost:8000/auth/me', {
       withCredentials: true
     })
-    console.log("✅ 已登入使用者:", res.data)
+    console.log("已登入使用者:", res.data)
 
-    const roleId = res.data.role_id
-
-    // 權限檢查
-    if (to.meta.requiresRole && to.meta.requiresRole !== roleId) {
-      alert('權限不足')
-      return next(false)
-    }
-
-    return next()
+    next()
   } catch (err) {
-    console.log("🚫 未登入或 cookie 無效，導回登入頁", err)
-    return next('/login')
+    console.log("未登入或 cookie 無效，導回登入頁", err)
+    next('/login')
   }
 })
 
