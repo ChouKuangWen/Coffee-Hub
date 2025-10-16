05eae3d14bb32567f59bc15209a4023cb7cfc098
Kuang-Wen,Chou <zx011035@gmail.com>
2025-10-16 14:49:54 +0800

    1.owner加上 lazy="joined" 2.並新增@property，其目的為:將 owner 關聯物件中的 email 欄位，給 Pydantic 序列化。 在查詢中使用了 joinedload，此時 self.owner 必然已被載入

--- backend/app/models/products.py
+++ backend/app/models/products.py
@@ -12,6 +12,17 @@ class Products(Base):
     description = Column(Text, nullable=True, comment='描述')
     # 商品擁有者
     owner_id = Column(Integer, ForeignKey("users.user_id", ondelete="CASCADE"), nullable=False, comment="商品擁有者 (賣家)")
-    owner = relationship("Users", back_populates="products")
+    owner = relationship("Users", back_populates="products" , lazy="joined")
+
+    @property
+    def owner_email(self) -> str | None:
+        """
+        將 owner 關聯物件中的 email 欄位，給 Pydantic 序列化。
+        在查詢中使用了 joinedload，此時 self.owner 必然已被載入。
+        """
+        # 存取已載入的 owner 關聯
+        if self.owner:
+            return self.owner.email
+        return None
 
 
