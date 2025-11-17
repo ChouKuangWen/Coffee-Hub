b67770e410a477264e1fc42483c62af617b3a655
Kuang-Wen,Chou <zx011035@gmail.com>
2025-11-17 20:18:52 +0800

    為了配合MySQL容器服務順利抓到環境變數，故調整為容器服務可以用變數

--- backend/app/models/base.py
+++ backend/app/models/base.py
@@ -5,11 +5,11 @@ import os
 load_dotenv()
 
 #定義連線資料庫的基本資訊
-db_user = os.getenv("db_user")
-db_password = os.getenv("db_password")
-db_host = os.getenv("db_host")
-db_port = os.getenv("db_port")
-db_name = os.getenv("db_name")
+db_user = os.getenv("DB_USER")
+db_password = os.getenv("DB_PASSWORD")
+db_host = os.getenv("DB_HOST")
+db_port = os.getenv("DB_PORT")
+db_name = os.getenv("DB_NAME")
 
 #SQLAlchemy 資料庫連線字串 mysql+asyncmy://帳號:密碼@主機:埠號/資料庫?參數
 db_url = f"mysql+asyncmy://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}?charset=utf8mb4"
