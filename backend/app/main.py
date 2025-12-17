35765aa647a99ddb82cc96f9c1f70b13d4d34354
Kuang-Wen,Chou <zx011035@gmail.com>
2025-12-17 13:26:27 +0800

    feat: 設定後端支援正式環境前端連線
    1. 加入 CORS 允許清單：http://[YOUR_BUCKET_NAME].storage.googleapis.com
    2. 修正環境變數讀取邏輯
    3. 確保 Cloud Run 部署後能正確處理來自外部的 API 請求

--- backend/app/main.py
+++ backend/app/main.py
@@ -43,7 +43,8 @@ app.add_middleware(
     allow_origins=["http://localhost:5173",
                    "http://localhost:3000",
                    "http://127.0.0.1:3000",
-                   "https://member-order-management-backend-system-362813485122.asia-east1.run.app"],  # 可改為前端網址，例如 http://localhost:3000
+                   "https://member-order-management-backend-system-818780839244.asia-east1.run.app",
+                   "http://frontend_member.storage.googleapis.com"],  # 可改為前端網址，例如 http://localhost:3000
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
